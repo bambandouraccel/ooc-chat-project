958ff24b921310865613f12acafae14d210cfbf4
bambandouraccel <125573511+bambandouraccel@users.noreply.github.com>
2024-11-28 15:22:38 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Configuration de CORS
+app.use(cors({
+    origin: 'https://ooc-frontend-clover-chat.apps.origins.heritage.africa',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
+    credentials: true, // Pour autoriser les cookies et en-têtes d'autorisation
+}));
